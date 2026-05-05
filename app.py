7e8ed165378e5c1bd09e4c863f760ee8f3e19799
Vlajko <vlada.gasevic@icloud.com>
2026-05-05 18:58:52 +0200

    Update containerPort to 5000 in deployment and service configurations; change app port to 5000 in Flask application

--- app.py
+++ app.py
@@ -52,4 +52,4 @@ def index():
                                  db_password=db_password)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5005)
+    app.run(host='0.0.0.0', port=5000)
